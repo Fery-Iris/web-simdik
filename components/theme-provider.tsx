fea2d4afba98cac4800803b7e823dd9a0df29d42
fery1509 <163739400+fery1509@users.noreply.github.com>
2025-09-25 10:34:35 +0800

    Fix merge conflicts in theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -5,8 +5,3 @@ import type { ThemeProviderProps } from "next-themes"
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
-<<<<<<< HEAD
-
-
-=======
->>>>>>> e782d4bf32546490bf693c01e4ed83320a5fb176
